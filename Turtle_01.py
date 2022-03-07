90d540ef1de91712ac5054bd2cbe5cecdc021bb8
Mridul kant Kaushik <78265624+Mridul-web@users.noreply.github.com>
2022-03-07 17:46:07 +0530

    commit

--- Turtle_01.py
+++ Turtle_01.py
@@ -1,5 +1,26 @@
-import turtle
-
-
-timmy = turtle.Turtle()
-
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# screen = Screen()
+# screen.screensize(2000, 1500)
+# timmy.shape("turtle")
+# timmy.speed(1)
+# timmy.color("green")
+# for x in range(1, 5):
+#     timmy.forward(200)
+#     timmy.left(90)
+# print(timmy)
+#
+# sh = Screen()
+# print(sh.canvheight)
+# print(sh.canvwidth)
+# sh.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",[ "Pikachu", "Pigi", "Eeve"])
+table.add_column("Type", ["Electric", "Flying", "Normal"])
+print(table.align)
+table.align = "l"
+print(table.align)
+#table.align["Pokemon Name"] = "r"
+print(table)
