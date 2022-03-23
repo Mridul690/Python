adedba4a8d0e7661194e42bbe5246bca1a33e8ae
Mridul kant Kaushik <78265624+Mridul-web@users.noreply.github.com>
2022-03-23 23:55:20 +0530

    commit

--- list.py
+++ list.py
@@ -1,16 +1,17 @@
-#Creating a multi-data types list
-#List are created using []
-c = [45,'Danger',False,32.2]
+# Creating a multi-data types list
+# List are created using []
+c = [45, 'Danger', False, 32.2]
 print(c)
 
-#List can be accessed through the index number 
+# List can be accessed through the index number
 print(c[3])
 
-#Lists values can be changed 
+# List values can be changed
 c[2] = True
 print(c)
 
-#List Slicing
-friends = ["Harry","Sam","Divya","Suty",45]
+# List Slicing
+# list[i:n]
+# From ith position to (n-1)th position
+friends = ["Harry", "Sam", "Divya", "Sty", 45]
 print(friends[0:4])
-
